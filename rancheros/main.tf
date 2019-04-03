2bdca38a61e83bbece6cff862b7f0ef2ce41e67f
Peter Pouliot <peter@pouliot.net>
2019-04-03 01:00:57 -0400

    converting rancheros due to qcow2 images as default

--- rancheros/main.tf
+++ rancheros/main.tf
@@ -1,9 +1,18 @@
 ## RancherOS
-resource "openstack_images_image_v2" "rancheros" {
-  name   = "RancherOS"
-  image_source_url = "https://releases.rancher.com/os/latest/rancheros-openstack.img"
+resource "null_resource" "download-extract-image-rancheros" {
+  provisioner "local-exec" {
+    command = "./ros_image.sh 1.5.1"
+  }
+}
+resource "openstack_images_image_v2" "rancheros_151_openstack" {
+  name   = "rancheros-1.5.1-openstack"
+  local_file_path = "${pathexpand("~/.terraform/image_cache/rancheros-1.5.1-openstack.raw")}"
   container_format = "bare"
   disk_format = "raw"
+  depends_on = [
+    "null_resource.download-extract-image-rancheros",
+  ]
+
   properties = {
     key = "value"
     os_distro = "rancheros"
---
+++ rancheros/ros_image.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+IMAGE_CACHE=$HOME/.terraform/image_cache
+RANCHEROS_RELEASE=$1
+
+PROJECT_DIR=`pwd`
+
+echo "Image Cache Location: "
+echo $IMAGE_CACHE
+echo
+echo "Project Directory: "
+echo $PROJECT_DIR
+echo
+
+cd $IMAGE_CACHE
+
+if [ -z "$1" ]
+then
+  echo "!! ---------------------------------------------------------------- !!
+!! ERROR: No RANCHEROS_RELEASE argument supplied to script. Please  !!
+!! supply the release number wanted ass an option to the script.    !!
+!!                                                                  !!
+!! USAGE: ./ros_image.sh 1.5.1                                      !!
+!! ---------------------------------------------------------------- !!"
+  cd $PROJECT_DIR
+  exit
+fi
+
+if [ -e rancheros-openstack.img ]
+then
+  echo "rancheros-openstack.img image found.  Removing image."
+  rm -rf rancheros-openstack.*
+else
+  echo "No previous RancherOS image found."
+fi
+wget https://github.com/rancher/os/releases/download/v$RANCHEROS_RELEASE/rancheros-openstack.img
+echo "Converting qcow image to raw"
+qemu-img convert -f qcow2 -O raw rancheros-openstack.img rancheros-$RANCHEROS_RELEASE-openstack.raw
+cd $PROJECT_DIR
