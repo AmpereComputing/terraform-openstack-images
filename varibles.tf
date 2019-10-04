67ba2ca6ebed99917adc8945b32800a5a31adf49
root <root@ampere01.pouliot.net>
2019-10-03 21:09:15 -0400

    updating so devuan works

--- varibles.tf
+++ varibles.tf
@@ -274,3 +274,11 @@ variable "enable_vyos_rolling_latest_amd64_raw" {
   description = "Enable "
   default     = 0
 }
+variable "enable_devuan_ascii_200_amd64_qcow2" {
+  description = "Enable Devuan ASCII 2.0.0 AMD64 QCOW2 Image"
+  default     = 0
+}
+variable "enable_devuan_acsii_200_amd64_raw" {
+  description = "Enable Devuan ASCII 2.0.0 AMD64 RAW Image"
+  default     = 0
+}
