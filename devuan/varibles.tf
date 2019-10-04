7e743d2566f4df4c40d54d9aac3cf16f6034c3dd
root <root@ampere01.pouliot.net>
2019-10-03 20:47:47 -0400

    fixing typo in devuan

--- devuan/varibles.tf
+++ devuan/varibles.tf
@@ -1,8 +1,8 @@
-variable "enable_duvean_acsii_200_amd64_qcow2" {
-  description = "Enable Duvean ASCII 2.0.0 AMD64 QCOW2 Image"
+variable "enable_devuan_acsii_200_amd64_qcow2" {
+  description = "Enable Devuan ASCII 2.0.0 AMD64 QCOW2 Image"
   default     = 0
 }
-variable "enable_duvean_acsii_200_amd64_raw" {
-  description = "Enable Duvean ASCII 2.0.0 AMD64 RAW Image"
+variable "enable_devuan_acsii_200_amd64_raw" {
+  description = "Enable Devuan ASCII 2.0.0 AMD64 RAW Image"
   default     = 0
 }
