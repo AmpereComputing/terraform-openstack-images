80cc76788f0e9b2ea9a71c9d06517ce7637c263a
Peter Pouliot <peter@pouliot.net>
2020-02-24 12:30:04 -0800

    fixing terraform 12 errors

--- coreos/main.tf
+++ coreos/main.tf
@@ -9,11 +9,11 @@ resource "null_resource" "download-extract-image-coreos-stable-amd64" {
 resource "openstack_images_image_v2" "coreos_current_stable_amd64_qcow2" {
   count = var.enable_coreos_current_stable_amd64_qcow2 ? 1:0
   name   = "coreos-current-stable-amd64-qcow2"
-  local_file_path = "${pathexpand("~/.terraform/image_cache/stable/coreos_production_openstack_image.img")}"
+  local_file_path = pathexpand("~/.terraform/image_cache/stable/coreos_production_openstack_image.img")
   container_format = "bare"
   disk_format = "qcow2"
   depends_on = [
-    "null_resource.download-extract-image-coreos-stable-amd64",
+    null_resource.download-extract-image-coreos-stable-amd64,
   ]
 
   properties = {
@@ -23,11 +23,11 @@ resource "openstack_images_image_v2" "coreos_current_stable_amd64_qcow2" {
 resource "openstack_images_image_v2" "coreos_current_00_stable_amd64_raw" {
   count = var.enable_coreos_current_stable_amd64_raw ? 1:0
   name   = "coreos-current-stable-amd64-raw"
-  local_file_path = "${pathexpand("~/.terraform/image_cache/stable/coreos_production_openstack_image.raw")}"
+  local_file_path = pathexpand("~/.terraform/image_cache/stable/coreos_production_openstack_image.raw")
   container_format = "bare"
   disk_format = "raw"
   depends_on = [
-    "null_resource.download-extract-image-coreos-stable-amd64",
+    null_resource.download-extract-image-coreos-stable-amd64,
   ]
 
   properties = {
@@ -45,11 +45,11 @@ resource "null_resource" "download-extract-image-coreos-alpha-amd64" {
 resource "openstack_images_image_v2" "coreos_current_alpha_amd64_qcow2" {
   count = var.enable_coreos_current_alpha_amd64_qcow2 ? 1:0
   name   = "coreos-current-alpha-amd64-qcow2"
-  local_file_path = "${pathexpand("~/.terraform/image_cache/alpha/coreos_production_openstack_image.img")}"
+  local_file_path = pathexpand("~/.terraform/image_cache/alpha/coreos_production_openstack_image.img")
   container_format = "bare"
   disk_format = "qcow2"
   depends_on = [
-    "null_resource.download-extract-image-coreos-alpha-amd64",
+    null_resource.download-extract-image-coreos-alpha-amd64,
   ]
 
   properties = {
@@ -60,11 +60,11 @@ resource "openstack_images_image_v2" "coreos_current_alpha_amd64_qcow2" {
 resource "openstack_images_image_v2" "coreos_current_alpha_amd64_raw" {
   count = var.enable_coreos_current_alpha_amd64_raw ? 1:0
   name   = "coreos-current-alpha-amd64-raw"
-  local_file_path = "${pathexpand("~/.terraform/image_cache/alpha/coreos_production_openstack_image.raw")}"
+  local_file_path = pathexpand("~/.terraform/image_cache/alpha/coreos_production_openstack_image.raw")
   container_format = "bare"
   disk_format = "raw"
   depends_on = [
-    "null_resource.download-extract-image-coreos-alpha-amd64",
+    null_resource.download-extract-image-coreos-alpha-amd64,
   ]
 
   properties = {
@@ -82,11 +82,11 @@ resource "null_resource" "download-extract-image-coreos-beta-amd64" {
 resource "openstack_images_image_v2" "coreos_current_beta_amd64_qcow2" {
   count = var.enable_coreos_current_alpha_amd64_qcow2 ? 1:0
   name   = "coreos-current-beta-amd64-qcow2"
-  local_file_path = "${pathexpand("~/.terraform/image_cache/beta/coreos_production_openstack_image.img")}"
+  local_file_path = pathexpand("~/.terraform/image_cache/beta/coreos_production_openstack_image.img")
   container_format = "bare"
   disk_format = "qcow2"
   depends_on = [
-    "null_resource.download-extract-image-coreos-beta-amd64",
+    null_resource.download-extract-image-coreos-beta-amd64,
   ]
 
   properties = {
@@ -97,11 +97,11 @@ resource "openstack_images_image_v2" "coreos_current_beta_amd64_qcow2" {
 resource "openstack_images_image_v2" "coreos_current_beta_amd64_raw" {
   count = var.enable_coreos_current_alpha_amd64_raw ? 1:0
   name   = "coreos-current-beta-amd64-raw"
-  local_file_path = "${pathexpand("~/.terraform/image_cache/beta/coreos_production_openstack_image.raw")}"
+  local_file_path = pathexpand("~/.terraform/image_cache/beta/coreos_production_openstack_image.raw")
   container_format = "bare"
   disk_format = "raw"
   depends_on = [
-    "null_resource.download-extract-image-coreos-beta-amd64",
+    null_resource.download-extract-image-coreos-beta-amd64,
   ]
 
   properties = {
