e508cf40ff0742f60fe37fd492b3773c5284b161
Peter Pouliot <peter@pouliot.net>
2020-02-24 11:35:51 -0800

    code cleanup for proper execution

--- flatcar/varibles.tf
+++ flatcar/varibles.tf
@@ -1,24 +1,32 @@
 variable "enable_flatcar_current_stable_amd64_qcow2" {
-  description = "Enable CoreOS Current Stable AMD64 QCOW2 Image"
+  description = "Enable Flatcar Linux Current Stable AMD64 QCOW2 Image"
   default     = 0
 }
 variable "enable_flatcar_current_stable_amd64_raw" {
-  description = "Enable CoreOS Current Stable AMD64 Raw Image"
+  description = "Enable Flatcar Linux Current Stable AMD64 Raw Image"
   default     = 0
 }
 variable "enable_flatcar_current_alpha_amd64_qcow2" {
-  description = "Enable CoreOS Current Alpha AMD64 QCOW2 Image"
+  description = "Enable Flatcar Linux Current Alpha AMD64 QCOW2 Image"
+  default     = 0
+}
+variable "enable_flatcar_current_alpha_arm64_qcow2" {
+  description = "Enable Flatcar Linux Current Alpha Arm64 QCOW2 Image"
   default     = 0
 }
 variable "enable_flatcar_current_alpha_amd64_raw" {
-  description = "Enable CoreOS Current Alpha AMD64 Raw Image"
+  description = "Enable Flatcar Linux Current Alpha AMD64 Raw Image"
+  default     = 0
+}
+variable "enable_flatcar_current_alpha_arm64_raw" {
+  description = "Enable Flatcar Linux Current Alpha Arm64 Raw Image"
   default     = 0
 }
 variable "enable_flatcar_current_beta_amd64_qcow2" {
-  description = "Enable CoreOS Current Beta AMD64 QCOW2 Image"
+  description = "Enable Flatcar Linux Current Beta AMD64 QCOW2 Image"
   default     = 0
 }
 variable "enable_flatcar_current_beta_amd64_raw" {
-  description = "Enable CoreOS Current Beta AMD64 Raw Image"
+  description = "Enable Flatcar Linux Current Beta AMD64 Raw Image"
   default     = 0
 }
