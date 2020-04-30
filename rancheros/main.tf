65fe0f6882976e3ab23537a52b0dbc14d0617531
Peter Pouliot <peter@pouliot.net>
2020-04-30 09:05:58 -0700

    updating rancheros to 1.5.5 and add variable for version

--- rancheros/main.tf
+++ rancheros/main.tf
@@ -3,13 +3,13 @@ resource "null_resource" "download-extract-image-rancheros" {
   count = var.enable_rancheros_openstack_amd64_raw ? 1:0
 
   provisioner "local-exec" {
-    command = "./ros_image.sh 1.5.4"
+    command = "./ros_image.sh ${var.ros_version}"
   }
 }
-resource "openstack_images_image_v2" "rancheros_openstack_154_amd64_raw" {
+resource "openstack_images_image_v2" "rancheros_openstack_amd64_raw" {
   count = var.enable_rancheros_openstack_amd64_raw ? 1:0
-  name   = "rancheros-1.5.4-openstack-amd64-raw"
-  local_file_path = pathexpand("~/.terraform/image_cache/rancheros-1.5.4-openstack.raw")
+  name   = "rancheros-${var.ros_version}-openstack-amd64-raw"
+  local_file_path = pathexpand("~/.terraform/image_cache/rancheros-${var.ros_version}-openstack.raw")
   container_format = "bare"
   disk_format = "raw"
   depends_on = [
@@ -22,10 +22,10 @@ resource "openstack_images_image_v2" "rancheros_openstack_154_amd64_raw" {
   }
 }
 
-resource "openstack_images_image_v2" "rancheros_openstack_154_amd64_qcow2" {
+resource "openstack_images_image_v2" "rancheros_openstack_amd64_qcow2" {
   count = var.enable_rancheros_openstack_amd64_qcow2 ? 1:0
-  name   = "rancheros-1.5.4-openstack-amd64-qcow2"
-  image_source_url = "https://github.com/rancher/os/releases/download/v1.5.4/rancheros-openstack.img"
+  name   = "rancheros-${var.ros_version}-openstack-amd64-qcow2"
+  image_source_url = "https://github.com/rancher/os/releases/download/v${var.ros_version}/rancheros-openstack.img"
   container_format = "bare"
   disk_format = "qcow2"
   properties = {
--- rancheros/varibles.tf
+++ rancheros/varibles.tf
@@ -7,4 +7,10 @@ variable "enable_rancheros_openstack_amd64_qcow2" {
   description = "Enable RancherOS AMD64 QCOW2 Image"
   default     = 0
 }
+variable "ros_version" {
+  type        = "string"
+  description = "RancherOS Version"
+  default     = "1.5.5"
+}
+
 
