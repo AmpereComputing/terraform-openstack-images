eead2e64b856d84261f093b38eaf5e0c90cbad0b
Peter Pouliot <peter@pouliot.net>
2020-11-12 09:31:30 -0800

    trying to fix vars

--- debian/varibles.tf
+++ debian/varibles.tf
@@ -1,46 +1,37 @@
 variable "enable_debian_9_amd64_qcow2" {
   description = "Enable Debian 9 AMD64 QCOW2 Image"
-  type        = bool
   default     = 0
 }
 variable "enable_debian_9_amd64_raw" {
   description = "Enable Debian 9 AMD64 Raw Image"
-  type        = bool
   default     = 0
 }
 variable "enable_debian_10_amd64_qcow2" {
   description = "Enable Debian 9 AMD64 Raw Image"
-  type        = bool
   default     = 0
 }
 variable "enable_debian_10_amd64_raw" {
   description = "Enable Debian 10 AMD64 Raw Image"
-  type        = bool
   default     = 0
 }
 variable "enable_debian_9_arm64_raw" {
   description = "Enable "
-  type        = bool
   default     = 0
 }
 variable "enable_debian_9_arm64_qcow2" {
   description = "Enable "
-  type        = bool
   default     = 0
 }
 variable "enable_debian_10_arm64_raw" {
   description = "Enable "
-  type        = bool
   default     = 0
 }
 variable "enable_debian_10_arm64_qcow2" {
   description = "Enable "
-  type        = bool
   default     = 0
 }
 
 variable "enable_debian_11_arm64_qcow2" {
   description = "Enable "
-  type        = bool
   default     = 0
 }
