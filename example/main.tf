75eb528e62fe9d32e8b2da46597793b02d1515ae
Peter Pouliot <peter@pouliot.net>
2020-11-12 12:16:40 -0800

    updating example

--- example/main.tf
+++ example/main.tf
@@ -1,8 +1,9 @@
+# Operating System Images for Glance using terraform-openstack-images module
 module "glance_images" {
   source = "github.com/amperecomputing/terraform-openstack-images"
   # ArchLinux
   enable_archlinux_latest_x86_64_raw         = false
-  enable_archlinux_latest_x86_64_qcow2       = true
+  enable_archlinux_latest_x86_64_qcow2       = false
   # Centos
   enable_centos_6_1901_x86_64_qcow2          = false
   enable_centos_6_1901_x86_64_raw            = false
@@ -16,10 +17,13 @@ module "glance_images" {
   enable_centos_8_aarch64_raw                = false
   enable_centos_8_2_aarch64_qcow2            = true
   enable_centos_8_2_aarch64_raw              = false
+  # Centos atomic
   enable_centos_atomic_host_x86_64_qcow2     = false
   enable_centos_atomic_host_x86_64_raw       = false
+  # Cirrus
   enable_cirros_040_x86_64_qcow2             = false
   enable_cirros_040_x86_64_raw               = false
+  # Debian
   enable_debian_9_amd64_qcow2                = false
   enable_debian_9_amd64_raw                  = false
   enable_debian_10_amd64_qcow2               = false
@@ -29,8 +33,10 @@ module "glance_images" {
   enable_debian_10_arm64_qcow2               = true
   enable_debian_10_arm64_raw                 = false
   enable_debian_11_arm64_qcow2               = true
+  # Devuan
   enable_devuan_acsii_200_amd64_raw          = false
   enable_devuan_ascii_200_amd64_qcow2        = false
+  # Fedora
   enable_fedora_atomic_29_aarch64_qcow2      = true
   enable_fedora_atomic_29_x86_64_qcow2       = false
   enable_fedora_atomic_29_x86_64_raw         = false
@@ -44,9 +50,9 @@ module "glance_images" {
   enable_fedora_cloud_base_32_x86_64_raw     = false
   enable_fedora_cloud_base_33_x86_64_qcow2   = false
   enable_fedora_cloud_base_33_x86_64_raw     = false
-  enable_fedora_server_29_aarch64_qcow2      = false
+  enable_fedora_server_29_aarch64_qcow2      = true
   enable_fedora_server_29_aarch64_raw        = false
-  enable_fedora_server_30_aarch64_qcow2      = false
+  enable_fedora_server_30_aarch64_qcow2      = true
   enable_fedora_server_30_aarch64_raw        = false
   enable_fedora_server_31_aarch64_qcow2      = true
   enable_fedora_server_31_aarch64_raw        = false
@@ -54,6 +60,7 @@ module "glance_images" {
   enable_fedora_server_32_aarch64_raw        = false
   enable_fedora_server_33_aarch64_qcow2      = true
   enable_fedora_server_33_aarch64_raw        = false
+  # Flatcar
   enable_flatcar_current_stable_amd64_qcow2  = false
   enable_flatcar_current_stable_amd64_raw    = false
   enable_flatcar_current_alpha_amd64_qcow2   = false
@@ -62,20 +69,25 @@ module "glance_images" {
   enable_flatcar_current_beta_amd64_raw      = false
   enable_flatcar_current_alpha_arm64_qcow2   = true
   enable_flatcar_current_alpha_arm64_raw     = false
+  # Freebsd
   enable_freebsd_120_release_amd64_qcow2     = false
   enable_freebsd_120_release_amd64_raw       = false
   enable_freebsd_120_release_aarch64_qcow2   = false
   enable_freebsd_120_release_aarch64_raw     = false
+  # Kali Linux
   enable_kali_linux_2019_3_amd64_qcow2       = false
   enable_kali_linux_2019_3_amd64_raw         = false
+  # OpenSuse
   enable_opensuse_leap_150_x86_64_qcow2      = false
   enable_opensuse_leap_150_x86_64_raw        = false
   enable_opensuse_leap_151_x86_64_qcow2      = false
   enable_opensuse_leap_151_x86_64_raw        = false
   enable_opensuse_151_aarch64_qcow2          = false
   enable_opensuse_151_aarch64_raw            = false
+  # RancherOS
   enable_rancheros_openstack_amd64_qcow2     = false
   enable_rancheros_openstack_amd64_raw       = false
+  # Ubuntu
   enable_ubuntu_2004_amd64_qcow2             = false
   enable_ubuntu_2004_amd64_raw               = false
   enable_ubuntu_1804_amd64_qcow2             = false
@@ -92,5 +104,6 @@ module "glance_images" {
   enable_ubuntu_1604_xenial_arm64_raw        = false
   enable_ubuntu_1404_trusty_arm64_qcow2      = false
   enable_ubuntu_1404_trusty_arm64_raw        = false
+  # Vyos
   enable_vyos_rolling_latest_amd64_raw       = false
 }
