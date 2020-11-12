4e6b843e4e0c699229161e84d448388834ac0b77
Peter Pouliot <peter@pouliot.net>
2020-11-12 10:24:19 -0800

    trying to move to better module format

--- varibles.tf
+++ varibles.tf
@@ -1,11 +1,11 @@
 variable "enable_archlinux_latest_x86_64_raw" {
   description = "Enable "
-  default     = 0
+# default     = 0
 }
 
 variable "enable_archlinux_latest_x86_64_qcow2" {
   description = "Enable "
-  default     = 0
+# default     = 0
 }
 
 variable "enable_centos_atomic_host_x86_64_qcow2" {
