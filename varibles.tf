a08b10d5dbf98398611acb3249231c2785be0349
Peter Pouliot <peter@pouliot.net>
2020-11-12 11:48:25 -0800

    refactor to tf module

--- varibles.tf
+++ varibles.tf
@@ -9,32 +9,40 @@ variable "enable_archlinux_latest_x86_64_qcow2" {
 }
 
 variable "enable_centos_atomic_host_x86_64_qcow2" {
-  description = "Enable Centos Atomic Host x86_64 QCOW2 Image"
+  description = "Enable Centos Atomic Host x86_64 qcow2 image"
   type        = bool
 }
 
 variable "enable_centos_atomic_host_x86_64_raw" {
-  description = "Enable Centos Atomic Host x86_64 Raw Image"
+  description = "Enable Centos Atomic Host x86_64 raw image"
   type        = bool
 }
 
 variable "enable_centos_7_x86_64_qcow2" {
-  description = "Enable Centos 7.2003 x86_64 QCOW2 Image"
+  description = "Enable Centos 7.2003 x86_64 qcow2 image"
   type        = bool
 }
 
 variable "enable_centos_7_x86_64_raw" {
-  description = "Enable Centos 7.2003 x86_64 Raw Image"
+  description = "Enable Centos 7.2003 x86_64 raw image"
+  type        = bool
+}
+variable "enable_centos_8_x86_64_qcow2 {
+  description = "Enable Centos 8 x86_64 qcow2 image"
+  type        = bool
+}
+variable "enable_centos_8_x86_64_raw" {
+  description = "Enable Centos 8 x86_64 raw image"
   type        = bool
 }
 
 variable "enable_centos_6_1901_x86_64_qcow2" {
-  description = "Enable Centos 6.1901 x86_64 QCOW2 Image"
+  description = "Enable Centos 6.1901 x86_64 qcow2 image"
   type        = bool
 }
 
 variable "enable_centos_6_1901_x86_64_raw" {
-  description = "Enable Centos 6.1901 x86_64 Raw Image"
+  description = "Enable Centos 6.1901 x86_64 raw image"
   type        = bool
 }
 
@@ -67,32 +75,32 @@ variable "enable_centos_8_2_aarch64_qcow2" {
 }
 
 variable "enable_cirros_040_x86_64_raw" {
-  description = "Enable CirrOS 0.4.0 x86_64 Raw Images"
+  description = "Enable CirrOS 0.4.0 x86_64 raw images"
   type        = bool
 }
 
 variable "enable_cirros_040_x86_64_qcow2" {
-  description = "Enable CirrOS 0.4.0 x86_64 QCOW2 Images"
+  description = "Enable CirrOS 0.4.0 x86_64 qcow2 images"
   type        = bool
 }
 
 variable "enable_debian_9_amd64_qcow2" {
-  description = "Enable Debian 9 AMD64 QCOW2 Image"
+  description = "Enable Debian 9 AMD64 qcow2 image"
   type        = bool
 }
 
 variable "enable_debian_9_amd64_raw" {
-  description = "Enable Debian 9 AMD64 Raw Image"
+  description = "Enable Debian 9 AMD64 raw image"
   type        = bool
 }
 
 variable "enable_debian_10_amd64_qcow2" {
-  description = "Enable Debian 9 AMD64 Raw Image"
+  description = "Enable Debian 9 AMD64 raw image"
   type        = bool
 }
 
 variable "enable_debian_10_amd64_raw" {
-  description = "Enable Debian 10 AMD64 Raw Image"
+  description = "Enable Debian 10 AMD64 raw image"
   type        = bool
 }
 
@@ -121,164 +129,211 @@ variable "enable_debian_11_arm64_qcow2" {
 }
 
 variable "enable_fedora_atomic_29_x86_64_qcow2" {
-  description = "Enable Fedora Atomic Host 29 x86_64 QCOW2 Images"
+  description = "Enable Fedora Atomic Host 29 x86_64 qcow2 images"
   type        = bool
 }
 
 variable "enable_fedora_atomic_29_x86_64_raw" {
-  description = "Enable Fedora Atomic Host 29 x86_64 RAW Images"
+  description = "Enable Fedora Atomic Host 29 x86_64 raw images"
+  type        = bool
+}
+variable "enable_fedora_atomic_29_aarch64_qcow2" {
+  description = "Enable Fedora Atomic Host 29 aarch64 qcow2 images"
   type        = bool
 }
 
+
 variable "enable_fedora_cloud_base_29_x86_64_qcow2" {
-  description = "Enable Fedora 29 x86_64 QCOW2 Images"
+  description = "Enable Fedora 29 x86_64 qcow2 images"
   type        = bool
 }
 
 variable "enable_fedora_cloud_base_29_x86_64_raw" {
-  description = "Enable Fedora 29 x86_64 Raw Images"
+  description = "Enable Fedora 29 x86_64 raw images"
   type        = bool
 }
 
 variable "enable_fedora_cloud_base_30_x86_64_qcow2" {
-  description = "Enable Fedora 30 x86_64 QCOW2 Images"
+  description = "Enable Fedora 30 x86_64 qcow2 images"
   type        = bool
 }
 
 variable "enable_fedora_cloud_base_30_x86_64_raw" {
-  description = "Enable Fedora 30 x86_64 Raw Images"
+  description = "Enable Fedora 30 x86_64 raw images"
   type        = bool
 }
 
 variable "enable_fedora_cloud_base_31_x86_64_qcow2" {
-  description = "Enable Fedora 31 x86_64 QCOW2 Images"
+  description = "Enable Fedora 31 x86_64 qcow2 images"
   type        = bool
 }
 
 variable "enable_fedora_cloud_base_31_x86_64_raw" {
-  description = "Enable Fedora 31 x86_64 Raw Images"
+  description = "Enable Fedora 31 x86_64 raw images"
   type        = bool
 }
 
 variable "enable_fedora_cloud_base_32_x86_64_qcow2" {
-  description = "Enable Fedora 32 x86_64 QCOW2 Images"
+  description = "Enable Fedora 32 x86_64 qcow2 images"
   type        = bool
 }
 
 variable "enable_fedora_cloud_base_32_x86_64_raw" {
-  description = "Enable Fedora 32 x86_64 Raw Images"
+  description = "Enable Fedora 32 x86_64 raw images"
   type        = bool
 }
 
 variable "enable_fedora_cloud_base_33_x86_64_qcow2" {
-  description = "Enable Fedora 33 x86_64 QCOW2 Images"
+  description = "Enable Fedora 33 x86_64 qcow2 images"
   type        = bool
 }
 
 variable "enable_fedora_cloud_base_33_x86_64_raw" {
-  description = "Enable Fedora 33 x86_64 Raw Images"
+  description = "Enable Fedora 33 x86_64 raw images"
   type        = bool
 }
 
 
 variable "enable_fedora_server_29_aarch64_raw" {
-  description = "Enable Fedora 29 AARCH Raw Images"
+  description = "Enable Fedora 29 AARCH raw images"
   type        = bool
 }
 
 variable "enable_fedora_server_29_aarch64_qcow2" {
-  description = "Enable Fedora 29 AARCH QCOW2 Images"
+  description = "Enable Fedora 29 AARCH qcow2 images"
   type        = bool
 }
 
 variable "enable_fedora_server_30_aarch64_raw" {
-  description = "Enable Fedora 30 AARCH Raw Images"
+  description = "Enable Fedora 30 AARCH raw images"
   type        = bool
 }
 
 variable "enable_fedora_server_30_aarch64_qcow2" {
-  description = "Enable Fedora 30 AARCH QCOW2 Images"
+  description = "Enable Fedora 30 AARCH qcow2 images"
+  type        = bool
+}
+
+variable "enable_fedora_server_31_aarch64_raw" {
+  description = "Enable Fedora 31 AARCH raw images"
+  type        = bool
+}
+
+variable "enable_fedora_server_31_aarch64_qcow2" {
+  description = "Enable Fedora 31 AARCH qcow2 images"
+  type        = bool
+}
+
+variable "enable_fedora_server_32_aarch64_raw" {
+  description = "Enable Fedora 32 AARCH raw images"
+  type        = bool
+}
+
+variable "enable_fedora_server_32_aarch64_qcow2" {
+  description = "Enable Fedora 32 AARCH qcow2 images"
+  type        = bool
+}
+
+variable "enable_fedora_server_33_aarch64_raw" {
+  description = "Enable Fedora 33 AARCH raw images"
+  type        = bool
+}
+
+variable "enable_fedora_server_33_aarch64_qcow2" {
+  description = "Enable Fedora 33 AARCH qcow2 images"
   type        = bool
 }
 
 variable "enable_flatcar_current_stable_amd64_qcow2" {
-  description = "Enable Flatcar Linux Current Stable AMD64 QCOW2 Image"
+  description = "Enable Flatcar Linux Current Stable AMD64 qcow2 image"
   type        = bool
 }
 
 variable "enable_flatcar_current_stable_amd64_raw" {
-  description = "Enable Flatcar Linux Current Stable AMD64 Raw Image"
+  description = "Enable Flatcar Linux Current Stable AMD64 raw image"
   type        = bool
 }
 
 variable "enable_flatcar_current_alpha_amd64_qcow2" {
-  description = "Enable Flatcar Linux Current Alpha AMD64 QCOW2 Image"
+  description = "Enable Flatcar Linux Current Alpha AMD64 qcow2 image"
   type        = bool
 }
 
 variable "enable_flatcar_current_alpha_amd64_raw" {
-  description = "Enable Flatcar Linux Current Alpha AMD64 Raw Image"
+  description = "Enable Flatcar Linux Current Alpha AMD64 raw image"
   type        = bool
 }
 
 variable "enable_flatcar_current_beta_amd64_qcow2" {
-  description = "Enable Flatcar Linux Current Beta AMD64 QCOW2 Image"
+  description = "Enable Flatcar Linux Current Beta AMD64 qcow2 image"
   type        = bool
 }
 
 variable "enable_flatcar_current_beta_amd64_raw" {
-  description = "Enable Flatcar Current Beta AMD64 Raw Image"
+  description = "Enable Flatcar Current Beta AMD64 raw image"
   type        = bool
 }
 
 
 variable "enable_flatcar_current_alpha_arm64_qcow2" {
-  description = "Enable Flatcar Linux Current Alpha Arm64 QCOW2 Image"
+  description = "Enable Flatcar Linux Current Alpha Arm64 qcow2 image"
   type        = bool
 }
 
 variable "enable_flatcar_current_alpha_arm64_raw" {
-  description = "Enable Flatcar Linux Current Alpha Arm64 Raw Image" 
+  description = "Enable Flatcar Linux Current Alpha Arm64 raw image" 
   type        = bool
 }
 
 variable "enable_freebsd_120_release_amd64_qcow2" {
-  description = "Enable FreeBSD 12.0 Release AMD64 QCOW2 Image"
+  description = "Enable FreeBSD 12.0 Release AMD64 qcow2 image"
   type        = bool
 }
 
 variable "enable_freebsd_120_release_amd64_raw" {
-  description = "Enable FreeBSD 12.0 Release AMD64 Raw Image"
+  description = "Enable FreeBSD 12.0 Release AMD64 raw image"
   type        = bool
 }
 
+variable "enable_freebsd_120_release_aarch64_qcow2" {
+  description = "Enable FreeBSD 12.0 Release aarch64 qcow2 image"
+  type        = bool
+}
+
+variable "enable_freebsd_120_release_aarch64_raw" {
+  description = "Enable FreeBSD 12.0 Release aarch64 raw image"
+  type        = bool
+}
+
+
+
 variable "enable_kali_linux_2019_3_amd64_qcow2" {
-  description = "Enable Kali Linux 2019.3 AMD64 QCOW2 Image"
+  description = "Enable Kali Linux 2019.3 AMD64 qcow2 image"
   type        = bool
 }
 
 variable "enable_kali_linux_2019_3_amd64_raw" {
-  description = "Enable Kali Linux 2019.3 AMD64 Raw Image"
+  description = "Enable Kali Linux 2019.3 AMD64 raw image"
   type        = bool
 }
 
 variable "enable_opensuse_leap_150_x86_64_qcow2" {
-  description = "Enable openSUSE Leap 15.0 x86_64 QCOW2 Images"
+  description = "Enable openSUSE Leap 15.0 x86_64 qcow2 images"
   type        = bool
 }
 
 variable "enable_opensuse_leap_150_x86_64_raw" {
-  description = "Enable openSUSE Leap 15.0 x86_64 RAW Images"
+  description = "Enable openSUSE Leap 15.0 x86_64 raw images"
   type        = bool
 }
 
 variable "enable_opensuse_leap_151_x86_64_qcow2" {
-  description = "Enable openSUSE Leap 15.1 x86_64 QCOW2 Images"
+  description = "Enable openSUSE Leap 15.1 x86_64 qcow2 images"
   type        = bool
 }
 
 variable "enable_opensuse_leap_151_x86_64_raw" {
-  description = "Enable openSUSE Leap 15.1 x86_64 RAW Images"
+  description = "Enable openSUSE Leap 15.1 x86_64 raw images"
   type        = bool
 }
 
@@ -293,72 +348,93 @@ variable "enable_opensuse_151_aarch64_qcow2" {
 }
 
 variable "enable_rancheros_openstack_amd64_raw" {
-  description = "Enable RancherOS AMD64 Raw Image"
+  description = "Enable RancherOS AMD64 raw image"
   type        = bool
 }
 
 variable "enable_rancheros_openstack_amd64_qcow2" {
-  description = "Enable RancherOS AMD64 QCOW2 Image"
+  description = "Enable RancherOS AMD64 qcow2 image"
   type        = bool
 }
 
+variable "enable_ubuntu_2004_amd64_qcow2" {
+  description = "Enable Ubuntu 20.04 AMD64 qcow2 image"
+  type        = bool
+}
+
+variable "enable_ubuntu_2004_amd64_raw" {
+  description = "Enable Ubuntu 20.04 AMD64 raw image"
+  type        = bool
+}
+
+
 variable "enable_ubuntu_1804_amd64_qcow2" {
-  description = "Enable Ubuntu 18.04 AMD64 QCOW2 Image"
+  description = "Enable Ubuntu 18.04 AMD64 qcow2 image"
   type        = bool
 }
 
 variable "enable_ubuntu_1804_amd64_raw" {
-  description = "Enable Ubuntu 18.04 AMD64 Raw Image"
+  description = "Enable Ubuntu 18.04 AMD64 raw image"
   type        = bool
 }
 
 variable "enable_ubuntu_1604_amd64_qcow2" {
-  description = "Enable Ubuntu 16.04 AMD64 QCOW2 Image"
+  description = "Enable Ubuntu 16.04 AMD64 qcow2 image"
   type        = bool
 }
 
 variable "enable_ubuntu_1604_amd64_raw" {
-  description = "Enable Ubuntu 18.04 AMD64 Raw Image"
+  description = "Enable Ubuntu 18.04 AMD64 raw image"
   type        = bool
 }
 
 variable "enable_ubuntu_1404_amd64_qcow2" {
-  description = "Enable Ubuntu 14.04 AMD64 QCOW2 Image"
+  description = "Enable Ubuntu 14.04 AMD64 qcow2 image"
   type        = bool
 }
 
 variable "enable_ubuntu_1404_amd64_raw" {
-  description = "Enable Ubuntu 14.04 AMD64 Raw Image"
+  description = "Enable Ubuntu 14.04 AMD64 raw image"
   type        = bool
 }
 
+variable "enable_ubuntu_2004_focal_arm64_raw" {
+  description = "Enable Ubuntu 20.04 arm64 raw image"
+  type        = bool
+}
+variable "enable_ubuntu_2004_focal_arm64_qcow2" {
+  description = "Enable Ubuntu 20.04 arm64 qcow2 image"
+  type        = bool
+}
+
+
 variable "enable_ubuntu_1804_bionic_arm64_raw" {
-  description = "Enable "
+  description = "Enable Ubuntu 18.04 Bionic arm64 raw image"
   type        = bool
 }
 
 variable "enable_ubuntu_1804_bionic_arm64_qcow2" {
-  description = "Enable "
+  description = "Enable Ubuntu 18.04 Bionic arm64 qcow2 image"
   type        = bool
 }
 
 variable "enable_ubuntu_1604_xenial_arm64_raw" {
-  description = "Enable "
+  description = "Enable Ubuntu 16.04 Xenial arm64 raw image"
   type        = bool
 }
 
 variable "enable_ubuntu_1604_xenial_arm64_qcow2" {
-  description = "Enable "
+  description = "Enable Ubuntu 16.04 Xenial arm64 qcow2 image"
   type        = bool
 }
 
 variable "enable_ubuntu_1404_trusty_arm64_raw" {
-  description = "Enable "
+  description = "Enable Ubuntu 14.04 Trusty arm64 raw image"
   type        = bool
 }
 
 variable "enable_ubuntu_1404_trusty_arm64_qcow2" {
-  description = "Enable "
+  description = "Enable Ubuntu 14.04 Trusty arm64 qcow2 image"
   type        = bool
 }
 
@@ -368,11 +444,11 @@ variable "enable_vyos_rolling_latest_amd64_raw" {
 }
 
 variable "enable_devuan_ascii_200_amd64_qcow2" {
-  description = "Enable Devuan ASCII 2.0.0 AMD64 QCOW2 Image"
+  description = "Enable Devuan ASCII 2.0.0 AMD64 qcow2 image"
   type        = bool
 }
 
 variable "enable_devuan_acsii_200_amd64_raw" {
-  description = "Enable Devuan ASCII 2.0.0 AMD64 RAW Image"
+  description = "Enable Devuan ASCII 2.0.0 AMD64 raw image"
   type        = bool
 }
