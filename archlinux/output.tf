d879dfb34eb7028ff1d33e61164f68ba4f4f6ed9
Peter Pouliot <peter@pouliot.net>
2020-11-12 10:22:07 -0800

    trying to move to better module format

---
+++ archlinux/output.tf
@@ -0,0 +1,8 @@
+
+output "enable_archlinux_latest_x86_64_raw" {
+  value = var.enable_archlinux_latest_x86_64_raw 
+}
+
+output "enable_archlinux_latest_x86_64_qcow2" {
+  value = var.enable_archlinux_latest_x86_64_qcow2
+}
--- archlinux/varibles.tf
+++ archlinux/varibles.tf
@@ -1,8 +1,8 @@
 variable "enable_archlinux_latest_x86_64_raw" {
-  description = "Enable "
+  description = "Enable archlinux latest x86_64 raw images"
   default     = 0
 }
 variable "enable_archlinux_latest_x86_64_qcow2" {
-  description = "Enable "
+  description = "Enable archlinux latest x86_64 qcow2 images"
   default     = 0
 }
