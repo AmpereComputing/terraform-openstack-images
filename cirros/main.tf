7c4cfc73f802866a3c1665c7cdfe855ec8e385c1
Peter Pouliot <peter@pouliot.net>
2021-02-23 13:59:54 -0500

    Update main.tf

--- cirros/main.tf
+++ cirros/main.tf
@@ -1,16 +1,16 @@
 ## Cirros
 
 
-resource "null_resource" "download-extract-image-cirros-040-x86_64" {
-  count = var.enable_cirros_040_x86_64_raw ? 1:0
+resource "null_resource" "download-extract-image-cirros-051-x86_64" {
+  count = var.enable_cirros_051_x86_64_raw ? 1:0
   provisioner "local-exec" {
     command = "${path.module}/cirros_image.sh"
   }
 }
-resource "openstack_images_image_v2" "cirros-040-x86_64-raw" {
-  count = var.enable_cirros_040_x86_64_raw ? 1:0
-  name   = "cirros-0.4.0-x86_64"
-  image_source_url = "https://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img"
+resource "openstack_images_image_v2" "cirros-051-x86_64-raw" {
+  count = var.enable_cirros_051_x86_64_raw ? 1:0
+  name   = "cirros-0.5.1-x86_64"
+  image_source_url = "https://download.cirros-cloud.net/0.5.1/cirros-0.5.1-x86_64-disk.img"
   container_format = "bare"
   disk_format = "raw"
   visibility = "public"
@@ -21,10 +21,10 @@ resource "openstack_images_image_v2" "cirros-040-x86_64-raw" {
     os_distro = "cirros"
   }
 }
-resource "openstack_images_image_v2" "cirros-040-x86_64-qcow2" {
-  count = var.enable_cirros_040_x86_64_qcow2 ? 1:0
-  name   = "cirros-0.4.0-x86_64"
-  image_source_url = "https://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img"
+resource "openstack_images_image_v2" "cirros-051-x86_64-qcow2" {
+  count = var.enable_cirros_051_x86_64_qcow2 ? 1:0
+  name   = "cirros-0.5.1-x86_64"
+  image_source_url = "https://download.cirros-cloud.net/0.5.1/cirros-0.5.1-x86_64-disk.img"
   container_format = "bare"
   disk_format = "qcow2"
   visibility = "public"
