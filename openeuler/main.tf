f6cdd23ea46317a0ba67ca410cb29660040dfa3b
Peter Pouliot <peter@pouliot.net>
2021-02-25 15:31:49 -0500

    fixing typo in count variable

--- openeuler/main.tf
+++ openeuler/main.tf
@@ -6,7 +6,7 @@ resource "null_resource" "download-extract-image-openeuler-2003-LTS-x86_64" {
   }
 }
 resource "openstack_images_image_v2" "openeuler-2003-x86_64-raw" {
-  count = var.enable_openeuler_2003_LTS_x86_64_raw ? 1:0
+  count = var.enable_openeuler_2003-LTS_x86_64_raw ? 1:0
   name   = "openEuler-20.03-LTS.x86_64.raw"
   local_file_path = pathexpand("~/.terraform/image_cache/openEuler-20.03-LTS.x86_64.raw")
   container_format = "bare"
@@ -21,7 +21,7 @@ resource "openstack_images_image_v2" "openeuler-2003-x86_64-raw" {
   }
 }
 resource "openstack_images_image_v2" "openeuler-2003-x86_64-qcow2" {
-  count = var.enable_openeuler_2003_LTS_x86_64_qcow2 ? 1:0
+  count = var.enable_openeuler_2003-LTS_x86_64_qcow2 ? 1:0
   name   = "openEuler-20.03-LTS.x86_64.qcow2"
   local_file_path = pathexpand("~/.terraform/image_cache/openEuler-20.03-LTS.x86_64.qcow2")
   container_format = "bare"
@@ -43,7 +43,7 @@ resource "null_resource" "download-extract-image-openeuler-2003-LTS-aarch64" {
   }
 }
 resource "openstack_images_image_v2" "openeuler-2003-aarch64-raw" {
-  count = var.enable_openeuler_2003_LTS_aarch64_raw ? 1:0
+  count = var.enable_openeuler_2003-LTS_aarch64_raw ? 1:0
   name   = "openEuler-20.03-LTS.aarch64.raw"
   local_file_path = pathexpand("~/.terraform/image_cache/openEuler-20.03-LTS.aarch64.raw")
   container_format = "bare"
@@ -58,7 +58,7 @@ resource "openstack_images_image_v2" "openeuler-2003-aarch64-raw" {
   }
 }
 resource "openstack_images_image_v2" "openeuler-2003-aarch64-qcow2" {
-  count = var.enable_openeuler_2003_LTS_aarch64_qcow2 ? 1:0
+  count = var.enable_openeuler_2003-LTS_aarch64_qcow2 ? 1:0
   name   = "openEuler-20.03-LTS.aarch64.qcow2"
   local_file_path = pathexpand("~/.terraform/image_cache/openEuler-20.03-LTS.aarch64.qcow2")
   container_format = "bare"
