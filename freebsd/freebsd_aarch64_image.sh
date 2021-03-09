0c83c0e3fb5d40efef5d8f89bbe1a90b6f42a5f1
Peter Pouliot <peter@pouliot.net>
2021-03-09 08:51:19 -0500

    updating FreeBSD to 13.0 RC1

--- freebsd/freebsd_aarch64_image.sh
+++ freebsd/freebsd_aarch64_image.sh
@@ -16,14 +16,14 @@ echo
 
 cd $IMAGE_CACHE
 
-if [ -e FreeBSD-13.0-BETA4-arm64-aarch64.qcow2.xz ]
+if [ -e FreeBSD-13.0-RC1-arm64-aarch64.qcow2.xz ]
 then
-  echo "FreeBSD-13.0-BETA4-arm64-aarch64.qcow2.xz image found.  Removing image."
-  rm -rf FreeBSD-13.0-BETA4-arm64*
+  echo "FreeBSD-13.0-RC1-arm64-aarch64.qcow2.xz image found.  Removing image."
+  rm -rf FreeBSD-13.0-RC1-arm64*
 else
   echo "No previous FreeBSD-13.0 Image Found."
 fi
-wget https://download.freebsd.org/ftp/releases/VM-IMAGES/13.0-BETA4/aarch64/Latest/FreeBSD-13.0-BETA4-arm64-aarch64.qcow2.xz -O FreeBSD-13.0-BETA4-arm64-aarch64.qcow2.xz
-unxz -v FreeBSD-13.0-BETA4-arm64-aarch64.qcow2.xz
-qemu-img convert -f qcow2 -O raw FreeBSD-13.0-BETA4-arm64-aarch64.qcow2 FreeBSD-13.0-BETA4-arm64-aarch64.raw
+wget https://download.freebsd.org/ftp/releases/VM-IMAGES/13.0-RC1/aarch64/Latest/FreeBSD-13.0-RC1-arm64-aarch64.qcow2.xz -O FreeBSD-13.0-RC1-arm64-aarch64.qcow2.xz
+unxz -v FreeBSD-13.0-RC1-arm64-aarch64.qcow2.xz
+qemu-img convert -f qcow2 -O raw FreeBSD-13.0-RC1-arm64-aarch64.qcow2 FreeBSD-13.0-RC1-arm64-aarch64.raw
 cd $PROJECT_DIR
